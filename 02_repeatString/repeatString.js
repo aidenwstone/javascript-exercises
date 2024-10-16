e8f456857e629b6f2aee8235051d49910a44add4
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-10-16 16:16:38 -0400

    Return ERROR if number is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function(stringToRepeat, repetitions) {
     let repeatedString = "";
 
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+
     for (let i = 0; i < repetitions; i++) {
         repeatedString += stringToRepeat;
     }
