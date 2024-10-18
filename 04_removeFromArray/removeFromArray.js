4d74579e83271ec52996bc727a195225415b95da
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-10-17 22:06:22 -0400

    Add for loop to move backwards through array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
 const removeFromArray = function(array) {
     for (let i = 1; i < arguments.length; i++) {
-        
+        for (let j = array.length - 1; j >= 0; j--) {
+            
+        }
     }
 
     return array;
