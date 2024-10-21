c45f88ffe5dacf72334f38b254e21bcb6195eae0
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-10-21 18:56:06 -0400

    Add loop to add numbers to sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function(startInt, endInt) {
     let sum = 0;
+
+    for (let num = startInt; num <= endInt; num++) {
+        sum += num;
+    }
 };
 
 // Do not edit below this line
