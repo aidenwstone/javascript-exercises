0df0abab207e1c047f0b3172ebab8e461eac6db3
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-10-21 20:05:12 -0400

    Return ERROR if paramter is not an integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,6 +4,9 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2)) { 
+        return "ERROR";
+    }
 
     let currNum = num1;
     do {
