17bcddf596fa81b857ba0fc2173e058d6b9338b5
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-05-14 16:03:05 -0400

    Update function to account for negative numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,14 @@
 const fibonacci = function(num) {
     let fibonacci = [1, 1];
 
+    if (num == 0) { return 0; };
+    if (num < 0) { return 'OOPS' };
+    
     for (let i = 1; i < num; i++) {
         fibonacci.push(fibonacci[i - 1] + fibonacci[i]);
     }
 
-    return num == 0 ? 0 : fibonacci[num - 1];
+    return fibonacci[num - 1]; 
 };
 
 // Do not edit below this line
