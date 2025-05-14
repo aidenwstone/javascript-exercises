63a8d10a3f70b1a5375f8edf7022aa8aa50c174b
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-05-14 14:47:35 -0400

    Update function to account for uppercase letters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (str) {
     let result = true;
 
     const re = /[^a-z]/g;
-    const newStr = str.replaceAll(re, '');
+    const newStr = str.toLowerCase().replaceAll(re, '');
 
     for (let i = 0; i <= newStr.length / 2; i++) {
         if (newStr[i] != newStr[newStr.length - (1 + i)]) {
