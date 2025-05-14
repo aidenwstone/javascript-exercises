887e91ac786898f42f57c89e190d44f8e853363f
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-05-14 14:04:02 -0400

    Implement palindromes function for single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let result = true;
 
+    for (let i = 0; i <= str.length / 2; i++) {
+        if (str[i] != str[str.length - (1 + i)]) {
+            result = false;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
